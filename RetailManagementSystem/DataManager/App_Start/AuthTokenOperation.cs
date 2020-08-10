80f4f145d308963e26e1248fad245a6bd4470013
Sam Lee <sflee36@yahoo.com.au>
2020-08-10 13:55:01 +0800

    Configured swashbuckle swagger options for auth, to get a token, and parameters to include a bearer token to make requests

---
+++ RetailManagementSystem/DataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,57 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace DataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            // add new path to swagger UI at /token
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                // new post operation with these attributes
+                post = new Operation
+                {
+                    // auth category
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        // data type of paramaters sent
+                        "application/x-www-form-urlencoded"
+                    },
+                    // parameter definitions
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
