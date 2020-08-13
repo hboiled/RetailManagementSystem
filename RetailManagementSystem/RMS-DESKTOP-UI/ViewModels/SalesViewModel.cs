c5e6e19f6179cbe6493dd78151af043d3ceaab5e
Sam Lee <sflee36@yahoo.com.au>
2020-08-13 21:44:14 +0800

    Added sales view and VM with UI controls

---
+++ RetailManagementSystem/RMS-DESKTOP-UI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS_DESKTOP_UI.ViewModels
+{
+    public class SalesViewModel: Screen
+    {
+		private BindingList<string> _products;
+
+		public BindingList<string> Products
+		{
+			get { return _products; }
+			set { 
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		private string _itemQuantity;
+
+		public string ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set { 
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		private BindingList<string> _cart;
+		public BindingList<string> Cart
+		{
+			get { return _cart; }
+			set
+			{
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+		public string SubTotal
+		{
+			get { 
+				return "$00.00";
+			}			
+		}
+
+		public string Tax
+		{
+			get
+			{
+				return "$00.00";
+			}
+		}
+
+		public string Total
+		{
+			get
+			{
+				return "$00.00";
+			}
+		}
+
+
+		public bool CanAddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public bool CanCheckOut
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public void AddToCart()
+		{
+
+		}
+
+		public void RemoveFromCart()
+		{
+
+		}
+
+		public void CheckOut()
+		{
+
+		}
+
+
+	}
+}
