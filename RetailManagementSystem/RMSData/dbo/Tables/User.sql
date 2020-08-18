320a3acf51f56d82614b766d88355c22f028991c
Sam Lee <sflee36@yahoo.com.au>
2020-08-18 11:37:18 +0800

    Added foreign key relationships

--- RetailManagementSystem/RMSData/dbo/Tables/User.sql
+++ RetailManagementSystem/RMSData/dbo/Tables/User.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[User]
 (
-    [Id] NVARCHAR(128) NOT NULL, 
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
     [FirstName] NCHAR(50) NOT NULL, 
     [LastName] NCHAR(50) NOT NULL, 
     [EmailAddress] NCHAR(256) NOT NULL, 
